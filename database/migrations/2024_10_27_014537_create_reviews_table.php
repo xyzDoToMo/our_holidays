a304da1c0e8b700fed50bef3d60a850b518ef75c
xyzDoToMo <tyusuke@keio.jp>
2024-10-27 13:30:37 +0900

    first commit

---
+++ database/migrations/2024_10_27_014537_create_reviews_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->id(user_id);
+            $table->id(category_id);
+            $table->string('title');
+            $table->string('body');
+            $table->string('event_title');
+            $table->string('event_body');
+            $table->string('event_time');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
